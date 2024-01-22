f035c2f13e1fa2a2cb56f985a88db1ef55e8a616
Chandimaisuru <isuruchandima92@gmail.com>
2024-01-22 12:32:57 +0530

    create hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-export default function App() {
-  
+import React from 'react'
+import About from './sections/About'
+import Contact from './sections/Contact'
+import Header from './sections/Header'
+import Hero from './sections/Hero'
+import Footer from './sections/Footer'
+import Pricing from './sections/Pricing'
+import Testimonials from './sections/Testimonials'
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Header/>
+      <Hero/>
+      
+    </>
   )
 }
+
+export default App
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import heroImg from '../assets/images/heroImg.png'
+
+function Hero() {
+  return (
+    
+    <section className='flex flex-col items-center justify-between w-full h-auto gap-4 lg:flex-row bg-slate-100 lg:h-screen'>
+     
+     <div className='flex flex-col items-start justify-center gap-8 px-10 py-10 lg:w-1/2 lg:px-20 lg:py-20'>
+           <h1 className='text-6xl font-bold text-green-600'>One-click solution for your static website.</h1>
+           <p className='text-2xl text-slate-90'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo eos deserunt architecto quas animi quia distinctio veritatis ullam optio voluptas?</p>
+           
+     </div>
+
+     <div className='flex justify-center gap-7'>
+         <button className='bg-green-800 text-white px-4 lg:px-8 py-3 rounded-md text-[18px] hover:bg-black hover:text-white cursor-pointer'>View More</button>
+     </div>
+
+    </section>
+  )
+}
+
+export default Hero
